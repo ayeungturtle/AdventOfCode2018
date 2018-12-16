79cc143eae201bd1f0e5e939d45c3adc6a4fb06b
Andrew Yeung <ayeung@iq-inc.com>
2018-12-16 09:10:39 -0800

    first commit

---
+++ AdventOfCode2018/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2018
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            oneA();
+        }
+
+        // Each line in the input file contains a + or - operator and an integer.
+        // Example lines:  +1, -2, +3, +1
+        // Find the output after all operators have been run in line order.
+        private static void oneA()
+        {
+            StreamReader stream = new StreamReader("inputOneA.txt");
+            int line;
+            int total = 0;
+            while ((int.TryParse(stream.ReadLine(), out line)))
+            {
+                total += line;
+            };
+            Console.WriteLine(total);
+        }
+    }
+}
